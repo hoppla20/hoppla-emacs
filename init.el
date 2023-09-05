ca4d9531fd02de6ac3a0710d30f139a36a70ab07
vicu <vincent.cui@wibu.de>
2023-09-05 17:04:59 +0200

    update

--- init.el
+++ init.el
@@ -13,19 +13,4 @@
   (load-file hoppla/user-file))
 ;; load hoppla
 (org-babel-load-file hoppla/hoppla-file)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(helpful which-key use-package ox-reveal org-sticky-header
-	     no-littering magit ivy-rich htmlize evil-collection
-	     counsel catppuccin-theme general doom-modeline
-	     all-the-icons)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
